108fdf5171b2d16f8452382b750d1374b577ab5d
Kacper <kacperrymkiewicz@gmail.com>
2022-11-21 16:11:34 +0100

    Update models

--- car_rental_project/car_rental/models.py
+++ car_rental_project/car_rental/models.py
@@ -28,19 +28,26 @@ class Customer(models.Model):
 class Rental(models.Model):
     pickup_date = models.DateField()
     return_date = models.DateField()
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, related_name='rentals', on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, related_name='rentals', on_delete=models.CASCADE)
     status = models.BooleanField()
+
+    def __str__(self):
+        return "From " + str(self.pickup_date) + " to " + str(self.return_date) + ": " + self.customer.surname + " - " + self.car.brand + " " + self.car.model
 
 
 class Accident(models.Model):
-    type = models.CharField(max_length=45)
+    COLLISION = 'C'
+    BREAKDOWN = 'B'
+    OTHER = 'O'
+    TYPES = ((COLLISION, 'Car collision'), (BREAKDOWN, 'Car breakdown'), (OTHER, 'Other accident'))
+    type = models.CharField(max_length=2, choices=TYPES, default=COLLISION)
     description = models.CharField(max_length=500)
-    rental = models.ForeignKey(Rental, on_delete=models.CASCADE)
+    rental = models.ForeignKey(Rental, related_name='accidents', on_delete=models.CASCADE)
 
 
 class Review(models.Model):
     stars = models.IntegerField()
     date = models.DateField()
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, related_name='reviews', on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, related_name='reviews', on_delete=models.CASCADE)
