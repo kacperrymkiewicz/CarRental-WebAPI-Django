dd9dde8c1a63abc76dbc3b1383c8b46f4fb35e96
Kacper <kacperrymkiewicz@gmail.com>
2022-11-28 13:58:18 +0100

    Models registration

--- car_rental_project/car_rental/admin.py
+++ car_rental_project/car_rental/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Car, Customer, Rental, Accident, Review
 
 # Register your models here.
+admin.site.register(Car)
+admin.site.register(Customer)
+admin.site.register(Rental)
+admin.site.register(Accident)
+admin.site.register(Review)
